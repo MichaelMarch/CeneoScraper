9db523a357e27d0401f72f0226715f034ef34244
MichaelMarch <s222074@student.uek.krakow.pl>
2022-05-13 12:59:48 +0200

    2022-05-13 MM

--- analyzer.py
+++ analyzer.py
@@ -1,10 +1,43 @@
 import os
 import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
 
 product_id = input("Please enter id of the product: ")
-
-#print(*[file[:file.rfind('.')] for file in os.listdir("./opinions/")], sep='\n')
+print(*[file[:file.rfind('.')]
+      for file in os.listdir("./opinions/")], sep='\n')
 
 opinions = pd.read_json(f"opinions/opinions_{product_id}.json")
+opinions["stars"] = opinions["stars"].map(
+    lambda x: float(x.split('/')[0].replace(",", ".")))
 
-print(opinions)
+opinions_count = len(opinions)
+pros_count = opinions["pros"].map(bool).sum()
+cons_count = opinions["cons"].map(bool).sum()
+average_score = opinions["stars"].mean().round(2)
+recomendation = opinions["recomendation"].value_counts(
+    dropna=False).sort_index().reindex(["Nie polecam", "Polecam", None], fill_value=0)
+
+recomendation.plot.pie(
+    label="",
+    autopct=lambda p: "{:.1f}%".format(round(p)) if p > 0 else "",
+    colors=["crimson", "forestgreen", "grey"],
+    labels=["Nie polecam", "Polecam", "Nie mam zdania"]
+)
+plt.title("Rekomendacje")
+plt.savefig(f"plots/plot_{product_id}_recomendations.png")
+plt.close()
+
+
+stars = opinions["stars"].value_counts().sort_index().reindex(
+    list(np.arange(0, 5.5, 0.5)))
+plot = stars.plot.bar(
+    color = "pink"
+)
+plt.title("Oceny produktu")
+plt.xlabel("Liczba gwiazdek")
+plt.ylabel("liczba opinii")
+plt.grid(True, axis="y")
+plt.xticks(rotation=0)
+plt.savefig(f"plots/plot_{product_id}_stars.png")
+plt.close()
--- scraper.py
+++ scraper.py
@@ -22,7 +22,7 @@ selectors = {
     "stars": ["span.user-post__score-count"],
     "content": ["div.user-post__text"],
     "pros": ["div[class$=\"positives\"] ~ div.review-feature__item", None, True],
-    "pros": ["div[class$=\"negatives\"] ~ div.review-feature__item", None, True],
+    "cons": ["div[class$=\"negatives\"] ~ div.review-feature__item", None, True],
     "useful": ["button.vote-yes > span"],
     "useless": ["button.vote-no > span"],
     "published": ["span.user-post__published > time:nth-child(1)", "datetime"],
